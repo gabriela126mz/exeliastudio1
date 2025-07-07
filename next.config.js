0f2fd921c62a846ab31ebf443a1497a908180871
Gabriela <gabrielamalasquez126@gmail.com>
2025-07-07 18:33:57 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://www.exeliastudio.com',
   generateRobotsTxt: true,
+  exclude: [
+    '/landingpage',   //  ← ruta vieja que da 404
+    '/contact',  
+    '/Services', //  ← añade todas las que quieras ocultar
+  ],
 };

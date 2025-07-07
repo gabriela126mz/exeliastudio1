6252d70151ffd213647a6aa3c983a3608ab7249c
Gabriela <gabrielamalasquez126@gmail.com>
2025-07-07 20:14:27 +0200

    manifest

--- components/MetaTags.jsx
+++ components/MetaTags.jsx
@@ -13,7 +13,7 @@ export default function MetaTags({
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content="https://www.exeliastudio.com/logoshare.png" />
       <meta property="og:url" content={url} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,11 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/512.png" />
+        <link rel="apple-touch-icon" href="/512.png" />
+        <meta name="theme-color" content="#ffffff" />
 
-        <link rel="icon" href="/favicon_v2.ico" />
 
       </Head>
 
